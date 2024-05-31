71bccaab5ff9a22c4d519a00d02372e8ec014ddf
Erick-Cavalcante <ec102974@gmail.com>
2024-05-31 07:03:14 +0000

    changes in gasolina.py's file and gasoline.png's graphic

--- gasolina.py
+++ gasolina.py
@@ -1,18 +1,15 @@
   # gasolina.py
-
-  import pandas as pd
-  import seaborn as sns
-  import matplotlib.pyplot as plt
-
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
   # Leitura dos dados
-  df = pd.read_csv('gasolina.csv')
-
+df = pd.read_csv('gasolina.csv')
+  
   # Criação do gráfico de linha
-  plt.figure(figsize=(10, 6))
-  sns.lineplot(data=df, x='dia', y='venda', marker='o')
-  plt.title('Preço Médio da Gasolina em São Paulo - Julho de 2021')
-  plt.xlabel('Dia')
-  plt.ylabel('Preço (R$)')
-  plt.grid(True)
-  plt.savefig('gasolina.png')
-  plt.show()
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x='dia', y='venda', marker='o')
+plt.title('média do preço da Gasolina em São Paulo - Julho de 2021')
+plt.xlabel('Day')
+plt.ylabel('gasoline price')
+plt.grid(True)
+plt.savefig('gasoline.png')
